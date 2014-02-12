23192694d2aba9c4e1df2f4ab6b71ef9c2117465
Jordan Craig <jordan.craig94@gmail.com>
2014-02-12 21:22:45 +0000

    Rewrite OA to have a frontend web gui for browsing images.

--- app.js
+++ app.js
@@ -4,9 +4,12 @@ var url = 'http://www.unsplash.com/',
     fs = require('fs'),
     date = require('datejs'),
     nodefs = require('node-fs'),
+    express = require('express'),
     imageURLs = [],
+    localImages = [],
     images = 'images/' + Date.today().getFullYear() +
     '/' + Date.today().getWeek() + '/';
+
 
 function createDirectory(path) {
   nodefs.mkdirSync(path,0777,true,function(err) {
@@ -31,11 +34,31 @@ function downloadImages() {
   createDirectory(images);
   for (url in imageURLs) {
     request(imageURLs[url])
-    .pipe(fs.createWriteStream(__dirname + '/' + images + '/' + url + '.jpg'))
+    .pipe(fs.createWriteStream(__dirname + '/public/' + images + '/' + url + '.jpg'))
     .on('close', function(){
-      console.log('Image Downloaded');
+
     });
+    localImages.push(images + '/' + url + '.jpg');
+    console.log(localImages);
   }
 }
 
 request(url, getHTML);
+
+var app = express();
+
+
+app.use(express.logger());
+app.use(express.static(__dirname + '/public'));
+app.engine('.html', require('ejs').__express);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'html');
+
+app.get('/', function(req, res) {
+  res.render('index', {
+    images: localImages
+  });
+});
+
+app.listen(3000);
+console.log('Express server listening on port 3000.');
